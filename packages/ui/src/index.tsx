660464b9fc151c11b9f65757ca532fc27769e3a6
hakim maaouia <hakimmaaouia@gmail.com>
2024-08-08 15:04:58 +0100

    feat: add index file to ui package

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,1 +1,15 @@
-export { default as Button } from "./button";
+// Components
+import Button from "./components/button";
+import Image from "./components/image";
+import Typo from "./components/typo";
+
+// Sections
+import Hero1 from "./sections/heros/hero1";
+import Hero2 from "./sections/heros/hero2";
+export { GET_HERO1_FRAGMENT } from "./sections/heros/hero1";
+export { GET_BUTTON_FRAGMENT } from "./components/button/button.graphql";
+export { GET_TYPO_FRAGMENT } from "./components/typo/typo.graphql";
+//export { Hero2 } from "./sections/heros/hero2";
+//export { Testimonial1 } from "./sections/testimonial/testimonial1";
+
+export { Button, Image, Typo, Hero1, Hero2 };
