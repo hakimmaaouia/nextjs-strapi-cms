c1171e41a4087fbe64c8be95f61c11da60ed0719
hakim maaouia <hakimmaaouia@gmail.com>
2024-08-14 15:43:29 +0100

    feat: :sparkles:

---
+++ packages/ui/src/layouts/footer/Footer.tsx
@@ -0,0 +1,104 @@
+import Button from "../../components/button";
+import Typo from "../../components/typo";
+
+import { FC } from "react";
+
+interface ILinkItem {
+  title: string;
+  href: string;
+  isExternal: boolean;
+}
+
+interface ILink {
+  title: string;
+  item: ILinkItem[];
+}
+
+interface IFooterProps {
+  description: string;
+  links: ILink[];
+}
+
+const Footer: FC<IFooterProps> = ({ description, links }) => {
+  return (
+    <section>
+      <div className="bg-white">
+        <div className="container py-12 lg:py-16">
+          <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+            <div className="text-white xl:col-span-1">
+              <Typo
+                as="h2"
+                color="black"
+                weight="bold"
+                size="p"
+                className="lg:pr-8"
+              >
+                wickedblocks
+              </Typo>
+              <Typo
+                as="p"
+                size="p"
+                className="mt-2 w-3/4 text-sm text-gray-500"
+              >
+                {description}
+              </Typo>
+            </div>
+            <div className="mt-12 grid md:grid-cols-2 grid-cols-1 g gap-8 xl:col-span-2 xl:mt-0">
+              {links.map((linkGroup, index) => (
+                <div key={index} className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <Typo
+                      as="span"
+                      color="black"
+                      className="text-sm font-bold uppercase tracking-wider"
+                    >
+                      {linkGroup.title}
+                    </Typo>
+                    <ul role="list" className="mt-4 space-y-1">
+                      {linkGroup.item.map((link, index) => (
+                        <li key={index}>
+                          <Button
+                            href={link.href}
+                            size="sm"
+                            variant="link"
+                            isExternal={link.isExternal}
+                            target={link.isExternal ? "_blank" : "_self"}
+                          >
+                            <Typo as="h3" size="p2" color="gray">
+                              {link.title}
+                            </Typo>
+                          </Button>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-50 md:py-12 py-6 sm:px-6">
+        <div className="container">
+          <div className="flex flex-wrap items-baseline">
+            <span className="mt-2">
+              <Typo
+                as="span"
+                size="p2"
+                color="gray"
+                className="flex text-nowrap"
+              >
+                Copyright © 2020 - 2021
+              </Typo>
+              <Typo as="span" size="p2" color="black">
+                @WickedTemplates
+              </Typo>
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
---
+++ packages/ui/src/layouts/footer/footer.stories.tsx
@@ -0,0 +1,133 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Footer from ".";
+
+const meta: Meta<typeof Footer> = {
+  title: "Components/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+  args: {
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    links: [
+      {
+        title: "Navigation",
+        item: [
+          { title: "Pricing", href: "./pricing.html", isExternal: false },
+          {
+            title: "All Templates",
+            href: "./templates.html",
+            isExternal: false,
+          },
+          {
+            title: "Landing Pages",
+            href: "./landing-pages.html",
+            isExternal: false,
+          },
+          {
+            title: "Next.js Templates",
+            href: "./nextjs.html",
+            isExternal: false,
+          },
+          {
+            title: "Full Templates",
+            href: "./multi-page.html",
+            isExternal: false,
+          },
+          { title: "Free Templates", href: "./demos.html", isExternal: false },
+          {
+            title: "Documentation",
+            href: "./documentation.html",
+            isExternal: false,
+          },
+          { title: "Customers", href: "#", isExternal: false },
+          {
+            title: "About",
+            href: "https://twitter.com/wickedtemplates/",
+            isExternal: true,
+          },
+        ],
+      },
+      {
+        title: "UI/UX & Dev",
+        item: [
+          {
+            title: "Wicked Blocks",
+            href: "https://www.wickedblocks.dev",
+            isExternal: true,
+          },
+          {
+            title: "Wicked Backgrounds",
+            href: "https://www.wickedbackgrounds.com/",
+            isExternal: true,
+          },
+          {
+            title: "Wicked Popup's",
+            href: "https://wickedpopups.com/",
+            isExternal: true,
+          },
+          {
+            title: "Colors & Fonts",
+            href: "https://www.colorsandfonts.com/.html",
+            isExternal: true,
+          },
+          {
+            title: "30 Days Of Tailwind CSS",
+            href: "https://30daysoftailwindcss.com/",
+            isExternal: true,
+          },
+          {
+            title: "Wicked VS Code",
+            href: "https://www.wvsc.dev/.html",
+            isExternal: true,
+          },
+          {
+            title: "Brutalist Websites",
+            href: "https://www.brutalist.one/.html",
+            isExternal: true,
+          },
+        ],
+      },
+      {
+        title: "Legal",
+        item: [
+          { title: "Changelog", href: "./changelog.html", isExternal: false },
+          { title: "FAQ", href: "./faq.html", isExternal: false },
+          { title: "Refund", href: "./refund.html", isExternal: false },
+          { title: "License", href: "./license.html", isExternal: false },
+          {
+            title: "Privacy Policy",
+            href: "./privacy.html",
+            isExternal: false,
+          },
+          { title: "Terms", href: "./terms.html", isExternal: false },
+        ],
+      },
+      {
+        title: "Socials",
+        item: [
+          {
+            title: "Twitter",
+            href: "https://twitter.com/WickedTemplates",
+            isExternal: true,
+          },
+          {
+            title: "Dribbble",
+            href: "https://dribbble.com/MichaelAndreuzza.html",
+            isExternal: true,
+          },
+          {
+            title: "Indie Hackers",
+            href: "https://www.indiehackers.com/product/wicked-templates",
+            isExternal: true,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {};
