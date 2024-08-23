ff62976a4cb1bd5026cec5f14c3746cf648b02cc
hakim maaouia <hakimmaaouia@gmail.com>
2024-08-23 16:25:42 +0100

    feat: :sparkles:

--- apps/cms/types/generated/components.d.ts
+++ apps/cms/types/generated/components.d.ts
@@ -11,44 +11,6 @@ export interface UtilSeo extends Schema.Component {
   };
 }
 
-export interface SectionHero2 extends Schema.Component {
-  collectionName: 'components_components_hero2s';
-  info: {
-    displayName: 'Hero2';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.Component<'component.typo'>;
-    description: Attribute.Component<'component.typo'>;
-    image: Attribute.Component<'component.image'>;
-    firstButton: Attribute.Component<'component.button'>;
-    secondButton: Attribute.Component<'component.button'>;
-  };
-}
-
-export interface SectionHero1 extends Schema.Component {
-  collectionName: 'components_section_hero1s';
-  info: {
-    displayName: 'Hero1';
-  };
-  attributes: {
-    title: Attribute.Component<'component.typo'>;
-    description: Attribute.Component<'component.typo'>;
-    firstButton: Attribute.Component<'component.button'>;
-    secondButton: Attribute.Component<'component.button'>;
-  };
-}
-
-export interface SectionCardSlider extends Schema.Component {
-  collectionName: 'components_section_card_sliders';
-  info: {
-    displayName: 'CardSlider';
-  };
-  attributes: {
-    title: Attribute.String;
-  };
-}
-
 export interface NavNavList extends Schema.Component {
   collectionName: 'components_nav_nav_lists';
   info: {
@@ -88,15 +50,60 @@ export interface NavDropdown extends Schema.Component {
   };
 }
 
+export interface SectionHero2 extends Schema.Component {
+  collectionName: 'components_components_hero2s';
+  info: {
+    displayName: 'Hero2';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.Component<'component.typo'>;
+    description: Attribute.Component<'component.typo'>;
+    firstButton: Attribute.Component<'component.button'>;
+    secondButton: Attribute.Component<'component.button'>;
+    className: Attribute.String;
+    image: Attribute.Media<'images'> & Attribute.Required;
+  };
+}
+
+export interface SectionHero1 extends Schema.Component {
+  collectionName: 'components_section_hero1s';
+  info: {
+    displayName: 'Hero1';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.Component<'component.typo'>;
+    description: Attribute.Component<'component.typo'>;
+    firstButton: Attribute.Component<'component.button'>;
+    secondButton: Attribute.Component<'component.button'>;
+    image: Attribute.Media<'images'> & Attribute.Required;
+    className: Attribute.String;
+  };
+}
+
+export interface SectionCardSlider extends Schema.Component {
+  collectionName: 'components_section_card_sliders';
+  info: {
+    displayName: 'CardSlider';
+  };
+  attributes: {
+    title: Attribute.String;
+  };
+}
+
 export interface ComponentTypo extends Schema.Component {
   collectionName: 'components_component_typos';
   info: {
     displayName: 'Typo';
+    description: '';
   };
   attributes: {
     as: Attribute.Enumeration<
       ['h1', 'h2', 'h3', 'h4', 'p', 'span', 'blockquote']
-    >;
+    > &
+      Attribute.Required &
+      Attribute.DefaultTo<'span'>;
     text: Attribute.Text & Attribute.Required;
     highlightedIndex: Attribute.String;
   };
@@ -139,12 +146,12 @@ declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'util.seo': UtilSeo;
-      'section.hero2': SectionHero2;
-      'section.hero1': SectionHero1;
-      'section.card-slider': SectionCardSlider;
       'nav.nav-list': NavNavList;
       'nav.link': NavLink;
       'nav.dropdown': NavDropdown;
+      'section.hero2': SectionHero2;
+      'section.hero1': SectionHero1;
+      'section.card-slider': SectionCardSlider;
       'component.typo': ComponentTypo;
       'component.link': ComponentLink;
       'component.image': ComponentImage;
